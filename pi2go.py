b528b89ba98e4b3e51e6820c2f273375dc335cbb
kd8bny <kd8bny@gmail.com>
2013-05-17 23:12:34 -0400

    Yayyy basic functions....ie it works up version number

--- pi2go.py
+++ pi2go.py
@@ -2,15 +2,30 @@
 #Daryl W. Bennett --kd8bny@gmail.com
 #Prupose is to have a DIY in car computer using RPi
 
-#V0 R0
+#V1 R0
 import sys
 import os
 
 from PyQt4 import *
 from main import *
 
-class fotoShoot(QtGui.QMainWindow):
+class pi2go(QtGui.QMainWindow):
 	def __init__(self, parent=None):
 		QtGui.QWidget.__init__(self, parent)
-		self.ui = Ui_Form()
+		self.ui = Ui_MainWindow()
 		self.ui.setupUi(self)
+		
+		QtCore.QObject.connect(self.ui.F_lights, QtCore.SIGNAL("clicked()"), self.fogL)	#browse button
+		QtCore.QObject.connect(self.ui.A_lights, QtCore.SIGNAL("clicked()"), self.fancy)	#browse button
+		
+	def fogL(self):	#Will turn fog ligths on and off
+		pass	
+		
+	def fancy(self):	#Will turn fog ligths on and off
+		pass		
+
+if __name__ == "__main__":
+    app = QtGui.QApplication(sys.argv)
+    myapp = pi2go()
+    myapp.show()
+    sys.exit(app.exec_())
