8186298bfd1b6c446c9f2195ce4fbc1d041d2e78
kd8bny <kd8bny@gmail.com>
2013-05-18 23:49:51 -0400

    Incorporated OBD II tab. Started calling classes from pyobd

--- pi2go.py
+++ pi2go.py
@@ -1,6 +1,7 @@
 #In car use of the raspberry pi
 #Daryl W. Bennett --kd8bny@gmail.com
 #Prupose is to have a DIY in car computer using RPi
+#TODO port back to RPi disabled now in order for serial testing
 
 #V1 R0
 import sys
@@ -8,7 +9,8 @@ import os
 
 from PyQt4 import *
 from main import *
-import RPi.GPIO as GPIO
+from obd_recorder import *
+#import RPi.GPIO as GPIO
 
 class pi2go(QtGui.QMainWindow):
 
@@ -21,8 +23,9 @@ class pi2go(QtGui.QMainWindow):
 		self.ui.setupUi(self)
 		
 		#Set buttons
-		QtCore.QObject.connect(self.ui.F_lights, QtCore.SIGNAL("clicked()"), self.fogL)	#browse button
-		QtCore.QObject.connect(self.ui.A_lights, QtCore.SIGNAL("clicked()"), self.fancy)	#browse button
+		QtCore.QObject.connect(self.ui.F_lights, QtCore.SIGNAL("clicked()"), self.fogL)	#fog lights
+		QtCore.QObject.connect(self.ui.A_lights, QtCore.SIGNAL("clicked()"), self.fancy)	#Accent lights
+		QtCore.QObject.connect(self.ui.obdStart, QtCore.SIGNAL("clicked()"), self.obdStart)	#Start OBD
 		
 		#Set logo
 		self.scene = QtGui.QGraphicsScene(self)
@@ -30,22 +33,30 @@ class pi2go(QtGui.QMainWindow):
 		self.ui.graphicsView.setScene(self.scene)
 		
 		#Set Hardware TODO Make class is enough features are introduced
-		GPIO.setup(self.F_lights, GPIO.OUT)
-		GPIO.setup(self.A_lights, GPIO.OUT)
-		GPIO.output(self.F_lights, False)
-		GPIO.output(self.A_lights, False)
+		#GPIO.setup(self.F_lights, GPIO.OUT)
+		#GPIO.setup(self.A_lights, GPIO.OUT)
+		#GPIO.output(self.F_lights, False)
+		#GPIO.output(self.A_lights, False)
 		
 	def fogL(self):	#Will turn fog ligths on and off
-			if(self.F_lights==False):
-				GPIO.output(self.F_lights, True)
-			else:
-				GPIO.output(self.F_lights, False)
-		
+		#if(self.F_lights==False):
+		#	GPIO.output(self.F_lights, True)
+		#else:
+		#	GPIO.output(self.F_lights, False)
+		pass
 	def fancy(self):	#Will turn fog ligths on and off
-		if(self.A_lights==False):
-				GPIO.output(self.A_lights, True)
-			else:
-				GPIO.output(self.A_lights, False)		
+		"""if(self.A_lights==False):
+			GPIO.output(self.A_lights, True)
+		else:
+			GPIO.output(self.A_lights, False)"""
+		pass
+	
+	def obdStart(self):
+		#Super basic function....just grabing a singal value for now
+		#rpmValue = obd_sensors.rpm(code)	#TODO grab code I think from IO YEP!!! TODO Now how??? #NOTE this may be obd_recorder instead parsing through logs
+		obd_recorder()
+		self.ui.lcdNumber.display(000)
+		
 
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
