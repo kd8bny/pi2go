4f45a02f1da8e7853b771f36f71f1160ca16b2d5
kd8bny <kd8bny@gmail.com>
2013-05-18 23:48:38 -0400

    Added OBD II tab

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'main.ui'
 #
-# Created: Sat May 18 00:08:01 2013
+# Created: Sat May 18 23:22:24 2013
 #      by: PyQt4 UI code generator 4.10
 #
 # WARNING! All changes made in this file will be lost!
@@ -35,15 +35,27 @@ class Ui_MainWindow(object):
         self.welcomeTab = QtGui.QWidget()
         self.welcomeTab.setObjectName(_fromUtf8("welcomeTab"))
         self.graphicsView = QtGui.QGraphicsView(self.welcomeTab)
-        self.graphicsView.setGeometry(QtCore.QRect(20, 30, 256, 192))
+        self.graphicsView.setGeometry(QtCore.QRect(20, 30, 238, 212))
         self.graphicsView.setObjectName(_fromUtf8("graphicsView"))
         self.label = QtGui.QLabel(self.welcomeTab)
         self.label.setGeometry(QtCore.QRect(440, 260, 91, 20))
         self.label.setObjectName(_fromUtf8("label"))
         self.tabWidget.addTab(self.welcomeTab, _fromUtf8(""))
-        self.musicTab = QtGui.QWidget()
-        self.musicTab.setObjectName(_fromUtf8("musicTab"))
-        self.tabWidget.addTab(self.musicTab, _fromUtf8(""))
+        self.obdTab = QtGui.QWidget()
+        self.obdTab.setObjectName(_fromUtf8("obdTab"))
+        self.obdStart = QtGui.QToolButton(self.obdTab)
+        self.obdStart.setGeometry(QtCore.QRect(480, 240, 51, 41))
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap(_fromUtf8(":/graphics/pyobd.gif")), QtGui.QIcon.Normal, QtGui.QIcon.On)
+        self.obdStart.setIcon(icon)
+        self.obdStart.setObjectName(_fromUtf8("obdStart"))
+        self.startOBD = QtGui.QLabel(self.obdTab)
+        self.startOBD.setGeometry(QtCore.QRect(385, 250, 81, 20))
+        self.startOBD.setObjectName(_fromUtf8("startOBD"))
+        self.lcdNumber = QtGui.QLCDNumber(self.obdTab)
+        self.lcdNumber.setGeometry(QtCore.QRect(60, 70, 81, 23))
+        self.lcdNumber.setObjectName(_fromUtf8("lcdNumber"))
+        self.tabWidget.addTab(self.obdTab, _fromUtf8(""))
         self.carTab = QtGui.QWidget()
         self.carTab.setObjectName(_fromUtf8("carTab"))
         self.F_lights = QtGui.QToolButton(self.carTab)
@@ -66,14 +78,17 @@ class Ui_MainWindow(object):
         MainWindow.setStatusBar(self.statusbar)
 
         self.retranslateUi(MainWindow)
-        self.tabWidget.setCurrentIndex(0)
+        self.tabWidget.setCurrentIndex(1)
+        QtCore.QObject.connect(self.obdStart, QtCore.SIGNAL(_fromUtf8("clicked()")), self.lcdNumber.setDecMode)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow", None))
-        self.label.setText(_translate("MainWindow", "pi2go V1 R0", None))
+        self.label.setText(_translate("MainWindow", "pi2go V1 R1", None))
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.welcomeTab), _translate("MainWindow", "Welcome", None))
-        self.tabWidget.setTabText(self.tabWidget.indexOf(self.musicTab), _translate("MainWindow", "Music", None))
+        self.obdStart.setText(_translate("MainWindow", "Start OBDII", None))
+        self.startOBD.setText(_translate("MainWindow", "Start OBDII", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.obdTab), _translate("MainWindow", "OBDII", None))
         self.F_lights.setText(_translate("MainWindow", "Fog Lights", None))
         self.A_lights.setText(_translate("MainWindow", "Accent Lights", None))
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.carTab), _translate("MainWindow", "Car", None))
