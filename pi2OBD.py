eba4aae859fd88cd76647917a26e530a5da6fb9d
kd8bny <kd8bny@gmail.com>
2013-05-29 18:02:09 -0400

    Workingsudo python2.7 pi2OBD.py! link between ODB and pi. Ditched pyOBD library

--- pi2OBD.py
+++ pi2OBD.py
@@ -4,40 +4,49 @@
 #Daryl W. Bennett --kd8bny@gmail.com
 #Prupose is to have a DIY in car computer using RPi
 
-#V1 R0
+#V1 R1
 
-import sys
-from optparse import OptionParser
-
-import obd
-from obd.message import OBDRequest
-from obd.util import untested
-import obd.util
+import serial
+import string
 
 class pi2OBD:
-	
+
 	def __init__(self):
-		#SetOBD #Find the scan tools attached to the computer and pick one
-		"""self.interfaces = obd.interface.elm.enumerate()
-		self.interface = self.interfaces[0]
-	
-		# Open the connection with the vehicle
-		self.interface.open()
-		self.interface.set_protocol(None)
-		self.interface.connect_to_vehicle()"""
-		pass
+		self.serialIO = serial.Serial('/dev/tty0', 38400, timeout=1)
+
+	def speed(self):
+		"""Grabs speed of vehicle"""
+		self.serialIO.write("01 0D \r")
+		speed_list = self.serialIO.readline().split(' ')
+		speed_hex = speed_list[0][4:6]
+		speed_float = float(int("0x"+speed_hex, 0))
 		
-	def OBDinter(self):
-		#Will grab all possible ports
-		pass
+		speed_float = speed_float*0.621371	#Comment out if you would rather have Kph
+		return speed_float
 	
+	def rpm(self):
+		"""Grabs RPM of Engine"""
+		self.serialIO.write("01 0C \r")
+		rpm_list = self.serialIO.readline().split(' ')
+		rpm_value = []
+		rpm_value.append(rpm_list[0][4:7])	#TODO test this value (Max 16)
+		rpm_value.append(rpm_list[1][4:7])	#TODO test this value (Max 383.75)
+		
+		rpm_value[0] = float(int("0x"+rpm_value[0], 0))
+		rpm_value[1] = float(int("0x"+rpm_value[1], 0))
+		
+		#Calulate RPM
+		rpm_final = ((rpm_value[0]*256)+rpm_value[1])*.25
+		return rpm_final
+		
 	def OBDread(self):
-		#Super basic function....just grabing a singal value for now
-		#rpmValue = obd_sensors.rpm(code)e
-		# Communicate with the vehicle
-		"""request = obd.message.OBDRequest(sid=0x01, pid=0x00)
-		responses = interface.send_request(request)""" #TODO working on
-		return 123 #test rpm value
+		while(1):
+			print self.speed()
+			print self.rpm()
+			time.sleep(10)
+		
 		
 if __name__ == "__main__":
-	pass
+	test = pi2OBD()
+	test.OBDread()
+	
