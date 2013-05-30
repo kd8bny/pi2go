cbf67aaa29074b0e423cf6cbc2edb6d9a3f383a8
kd8bny <kd8bny@gmail.com>
2013-05-29 20:33:17 -0400

    Small updates

--- pi2OBD.py
+++ pi2OBD.py
@@ -13,13 +13,14 @@ import time
 class pi2OBD:
 
 	def __init__(self):
-		self.serialIO = serial.Serial('/dev/rfcomm0', 38400, timeout=1)
+			self.serialIO = serial.Serial('/dev/rfcomm0', 38400, timeout=1)
 
 	def speed(self):
 		"""Grabs speed of vehicle"""
 		self.serialIO.write("01 0D \r")
 		speed_list = self.serialIO.readline().split(' ')
 		speed_hex = speed_list[0][4:6]
+		print speed_hex
 		speed_float = float(int("0x"+speed_hex, 0))
 		
 		speed_float = speed_float*0.621371	#Comment out if you would rather have Kph
@@ -30,8 +31,8 @@ class pi2OBD:
 		self.serialIO.write("01 0C \r")
 		rpm_list = self.serialIO.readline().split(' ')
 		rpm_value = []
-		rpm_value.append(rpm_list[0][4:6])	#TODO test this value (Max 16)
-		rpm_value.append(rpm_list[0][6:8])	#TODO test this value (Max 383.75)
+		rpm_value.append(rpm_list[0][4:6])
+		rpm_value.append(rpm_list[0][6:8])
 		
 		rpm_value[0] = float(int("0x"+rpm_value[0], 0))
 		rpm_value[1] = float(int("0x"+rpm_value[1], 0))
@@ -41,10 +42,11 @@ class pi2OBD:
 		return rpm_final
 		
 	def OBDread(self):
+		finalValues = [0,0] # [speed, rpm] #TODO maybe dictionary
 		while(1):
-			print self.speed()
-			print self.rpm()
-			time.sleep(10)
+			finalValues[0] = self.speed()
+			finalValues[1] = self.rpm()
+			return finalValues
 		
 		
 if __name__ == "__main__":
