c192540f1f0b3fbc3e8e44c4cb448c3819b9011d
kd8bny <kd8bny@gmail.com>
2014-06-01 22:23:44 -0400

    Major changes See CHANGELOG

--- pi2OBD.py
+++ pi2OBD.py
@@ -6,155 +6,124 @@
 
 #V2 R0a
 
-import serial, string, time, threading
+import serial, string, time
 from PyQt4.QtCore import *
 from main import *
+import config
 
 class pi2OBD(QObject):
-	global serialDevice, lineNumber
 
-	def __init__(self):
-		QObject.__init__(self)
+    def __init__(self):
+        QObject.__init__(self)
 
-		try:
-			self.serialIO = serial.Serial(serialDevice, 38400, timeout=1)
-		except:
-			print "Serial Issue"
+        try:
+            self.serialIO = serial.Serial('/dev/pts/6', 38400, timeout=1) #serialDevice
+        except:
+            print "Serial Issue"
+        
+    def speed(self):
+        """Grabs speed of vehicle"""
+        self.serialIO.write("01 0D \r")
+        speed_list = self.serialIO.readline().split(' ')
+        speed_hex = speed_list[0][4:6]
+        try:
+            speed_float = float(int("0x"+speed_hex, 0))
+        except:
+            return 0
+        
+        speed_float = speed_float*0.621371  #Comment out if you would rather have Kph
+        return speed_float
+    
+    def rpm(self):
+        """Grabs RPM of Engine"""
+        self.serialIO.write("01 0C \r")
+        rpm_list = self.serialIO.readline().split(' ')
+        rpm_value = []
+        rpm_value.append(rpm_list[0][4:6])
+        rpm_value.append(rpm_list[0][6:8])
+        try:
+            rpm_value[0] = float(int("0x"+rpm_value[0], 0))
+            rpm_value[1] = float(int("0x"+rpm_value[1], 0))
+        except:
+            return 0
+            
+        #Calulate RPM
+        rpm_final = ((rpm_value[0]*256)+rpm_value[1])*.25
+        return rpm_final
+        
+    def intake_temp(self):
+        """Grabs Intake Air temp (Temp outside)"""
+        self.serialIO.write("01 0F \r")
+        temp_list = self.serialIO.readline().split(' ')
+        temp_hex = temp_list[0][4:6]
+        try:
+            temp_float = float(int("0x"+temp_hex, 0))
+        except:
+            return 0
+            
+        temp_final = temp_float-40  
+        temp_final = temp_final*(9/5)+32    #Comment out if you would rather have deg C 
+        return temp_final
+        
+    def coolant_temp(self):
+        """Grabs Intake Air temp (Temp outside)"""
+        self.serialIO.write("01 05 \r")
+        temp_list = self.serialIO.readline().split(' ')
+        temp_hex = temp_list[0][4:6]
+        try:
+            temp_float = float(int("0x"+temp_hex, 0))
+        except:
+            return 0
+            
+        temp_final = temp_float-40
+        temp_final = temp_final*(9/5)+32    #Comment out if you would rather have deg C
+        return temp_final
+        
+    def load(self):
+        """Grabs Total Engine Load"""
+        self.serialIO.write("01 04 \r")
+        load_list = self.serialIO.readline().split(' ')
+        load_hex = load_list[0][4:6]
+        try:
+            load_float = float(int("0x"+load_hex, 0))
+        except:
+            return 0
+            
+        load_final = (load_float*100)/255
+        return load_final
+        
+    def run_time(self): #TODO
+        """Will grab amount of time engine has been running"""
+        self.serialIO.write("01 7F \r")
+        temp_list = self.serialIO.readline().split(' ')
+        pass
+        
+    def OBDread(self,oldvalues):
+        """Function to read and write data: [speed, rpm, intake, coolant, load]"""
+        OBDvalues = [0, 0, 0, 0, 0]
+        OBDvalues[0] = self.speed()
+        OBDvalues[1] = self.rpm()
+        OBDvalues[2] = self.intake_temp()
+        OBDvalues[3] = self.coolant_temp()
+        OBDvalues[4] = self.load()
 
-		#Datafile setup
-		self.obdTemp = open('obdTemp.txt', 'w')
+        return OBDvalues
+        
+    def clear_codes(self):  #TODO Will add to own class when working with codes
+        """Clear all trouble codes"""
+        self.serialIO.write("04")
 
-		#Thread Info
-		self.signal = SIGNAL("signal")
-		self.toKill = False
-		self.isFirst = True
-		
-	def speed(self):
-		"""Grabs speed of vehicle"""
-		self.serialIO.write("01 0D \r")
-		speed_list = self.serialIO.readline().split(' ')
-		speed_hex = speed_list[0][4:6]
-		try:
-			speed_float = float(int("0x"+speed_hex, 0))
-		except:
-			return 0
-		
-		speed_float = speed_float*0.621371	#Comment out if you would rather have Kph
-		return speed_float
-	
-	def rpm(self):
-		"""Grabs RPM of Engine"""
-		self.serialIO.write("01 0C \r")
-		rpm_list = self.serialIO.readline().split(' ')
-		rpm_value = []
-		rpm_value.append(rpm_list[0][4:6])
-		rpm_value.append(rpm_list[0][6:8])
-		try:
-			rpm_value[0] = float(int("0x"+rpm_value[0], 0))
-			rpm_value[1] = float(int("0x"+rpm_value[1], 0))
-		except:
-			return 0
-			
-		#Calulate RPM
-		rpm_final = ((rpm_value[0]*256)+rpm_value[1])*.25
-		return rpm_final
-		
-	def intake_temp(self):
-		"""Grabs Intake Air temp (Temp outside)"""
-		self.serialIO.write("01 0F \r")
-		temp_list = self.serialIO.readline().split(' ')
-		temp_hex = temp_list[0][4:6]
-		try:
-			temp_float = float(int("0x"+temp_hex, 0))
-		except:
-			return 0
-			
-		temp_final = temp_float-40	
-		temp_final = temp_final*(9/5)+32	#Comment out if you would rather have deg C 
-		return temp_final
-		
-	def coolant_temp(self):
-		"""Grabs Intake Air temp (Temp outside)"""
-		self.serialIO.write("01 05 \r")
-		temp_list = self.serialIO.readline().split(' ')
-		temp_hex = temp_list[0][4:6]
-		try:
-			temp_float = float(int("0x"+temp_hex, 0))
-		except:
-			return 0
-			
-		temp_final = temp_float-40
-		temp_final = temp_final*(9/5)+32	#Comment out if you would rather have deg C
-		return temp_final
-		
-	def load(self):
-		"""Grabs Total Engine Load"""
-		self.serialIO.write("01 04 \r")
-		load_list = self.serialIO.readline().split(' ')
-		load_hex = load_list[0][4:6]
-		try:
-			load_float = float(int("0x"+load_hex, 0))
-		except:
-			return 0
-			
-		load_final = (load_float*100)/255
-		return load_final
-		
-	def run_time(self):	#TODO
-		"""Will grab amount of time engine has been running"""
-		self.serialIO.write("01 7F \r")
-		temp_list = self.serialIO.readline().split(' ')
-		pass
-		
-	def OBDread(self):
-		"""Function to read and write data"""
-		try:
-			self.obdTemp.seek(lineNumber)
-			self.obdTemp.write(str(self.speed())+',')
-			self.obdTemp.write(str(self.rpm())+',')
-			self.obdTemp.write(str(self.intake_temp())+',')
-			self.obdTemp.write(str(self.coolant_temp())+',')
-			self.obdTemp.write(str(self.load())+','+'\n')
-		finally:
-			self.obdTemp.close()
-		return 
-		
-	def clear_codes(self):	#TODO Will add to own class when working with codes
-		"""Clear all trouble codes"""
-		self.serialIO.write("04")
-
-	def setup(self):
-		"""Set up OBD Comm"""
-		self.serialIO.write("ATZ \r")
-		time.sleep(2)
-		self.serialIO.write("ATSP %d \r" %pi2go.ATSP)
-		time.sleep(.5)
-		self.serialIO.readline().split(' ')
-		self.serialIO.write("01 00 \r")
-		time.sleep(.5)
-		return
-
-	def main(self,kill):
-		"""Formats OBDII data"""
-
-		if self.isFirst:
-			obdValue = self.setup()
-			self.isFirst = False
-			self.updateThread()
-
-		#Start thread
-		obdThread = threading.Thread(target = self.main)
-		obdThread.start()
-
-		while not (kill):
-			if(self.obdTemp.tell() > 2000):	#100 lines of data @20char
-				self.obdTemp.seek(0)
-			else:
-				self.wait(1)
-		self.updateThread.quit()
-		self.quit()
-		
+    def setup(self):
+        """Set up OBD Comm"""
+        self.serialIO.write("ATZ \r")
+        time.sleep(2)
+        self.serialIO.write("ATSP 1 \r")  #self.serialIO.write("ATSP %d \r" %pi2go.ATSP)
+        time.sleep(.5)
+        self.serialIO.readline().split(' ')
+        self.serialIO.write("01 00 \r")
+        time.sleep(.5)
+        return
+        
 if __name__ == "__main__":
-	test = pi2OBD()
-	test.main(False)
-	
+    test = pi2OBD()
+    test.main()
--- pi2go.py
+++ pi2go.py
@@ -11,153 +11,153 @@
 
 #V1 R5
 
-import sys, os, threading, PyQt4
+import sys, os, threading, PyQt4, time
 import PyQt4.Qwt5 as Qwt
-import pi2OBD #,sOff
+import pi2OBD, config #,sOff
 from main import *
 
 try:
-	import RPi.GPIO as GPIO
+    import RPi.GPIO as GPIO
 except:
-	print "Developmental Use Only"
-	pass
+    print "Developmental Use Only"
+    pass
 
 
 class pi2go(QtGui.QMainWindow):
-	global serialDevice, lineNumber
-    
-	def __init__(self, parent=None):
-		QtGui.QWidget.__init__(self, parent)
-		self.ui = Ui_MainWindow()
-		self.ui.setupUi(self)
 
-		lineNumber = 0
-		
-		try:
-			#Set Hardware TODO Make class is enough features are introduced
-			GPIO.setmode(GPIO.BCM)
-			self.F_lights = 17
-			self.A_lights = 22
-			GPIO.setup(self.F_lights, GPIO.OUT)
-			GPIO.setup(self.A_lights, GPIO.OUT)
-			GPIO.output(self.F_lights, GPIO.LOW)	; self.F_state = False
-			GPIO.output(self.A_lights, GPIO.LOW)	; self.A_state = False
-		except:
-			print 'Heads up: No GPIO Connection'
+    trigger = QtCore.pyqtSignal([list], name='speedChange')
+
+    def __init__(self, parent=None):
+        QtGui.QWidget.__init__(self, parent)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        
+        try:
+            #Set Hardware TODO Make class is enough features are introduced
+            GPIO.setmode(GPIO.BCM)
+            self.F_lights = 17
+            self.A_lights = 22
+            GPIO.setup(self.F_lights, GPIO.OUT)
+            GPIO.setup(self.A_lights, GPIO.OUT)
+            GPIO.output(self.F_lights, GPIO.LOW)    ; self.F_state = False
+            GPIO.output(self.A_lights, GPIO.LOW)    ; self.A_state = False
+        except:
+            print 'Heads up: No GPIO Connection'
 
 
-		#Qt4
-		#Welcome tab
-		self.scene = QtGui.QGraphicsScene(self)
-		self.scene.addPixmap(QtGui.QPixmap('graphics/pi_logo.jpeg'))
-		self.ui.welcome.setScene(self.scene)
+        #Qt4
+        #Welcome tab
+        self.scene = QtGui.QGraphicsScene(self)
+        self.scene.addPixmap(QtGui.QPixmap('graphics/pi_logo.jpeg'))
+        self.ui.welcome.setScene(self.scene)
 
-		self.timer = QtCore.QTimer()
-		QtCore.QObject.connect(self.timer, QtCore.SIGNAL("timeout()"), self.ui.AnalogClock, QtCore.SLOT("setCurrentTime()"))
-		self.timer.start(1000)
+        self.piClocktimer = QtCore.QTimer()
+        QtCore.QObject.connect(self.piClocktimer, QtCore.SIGNAL("timeout()"), self.ui.AnalogClock, QtCore.SLOT("setCurrentTime()"))
+        self.piClocktimer.start(1000)
 
-		#Car Tab
-		QtCore.QObject.connect(self.ui.F_lights, QtCore.SIGNAL("clicked()"), self.fogL)	#fog lights
-		QtCore.QObject.connect(self.ui.A_lights, QtCore.SIGNAL("clicked()"), self.fancy)	#Accent lights
-		
-		#OBD Tab
-		QtCore.QObject.connect(self.ui.obdButton, QtCore.SIGNAL("clicked()"), self.ODBII)	#Start/kill OBD
-		self.stopOBD = False
-		QtCore.QObject.connect(self.ui.obdClear, QtCore.SIGNAL("clicked()"), self.clearCodes) #clear codes
+        #Car Tab
+        QtCore.QObject.connect(self.ui.F_lights, QtCore.SIGNAL("clicked()"), self.fogL) #fog lights
+        QtCore.QObject.connect(self.ui.A_lights, QtCore.SIGNAL("clicked()"), self.fancy)    #Accent lights
+        
+        #OBD Tab
+        self.trigger.connect(self.updateGUI)
+        QtCore.QObject.connect(self.ui.obdButton, QtCore.SIGNAL("clicked()"), self.ODBII)   #Start/kill OBD
+        QtCore.QObject.connect(self.ui.obdClear, QtCore.SIGNAL("clicked()"), self.clearCodes) #clear codes
+        self.stopOBD = True #init stopped
 
-		#maybe?? 
-		#QtCore.QObject.connect(self.ui.obdClear, QtCore.SIGNAL("clicked()"), pi2OBD.pi2OBD, QtCore.SLOT("clear_codes()")) #clear codes
-		
-		#GPS tab
-		QtCore.QObject.connect(self.ui.logGPS, QtCore.SIGNAL("clicked()"), self.logGPS)
-		QtCore.QObject.connect(self.ui.GPSbutton, QtCore.SIGNAL("clicked()"), self.GPS)
-		self.stopGPS = False
-		
-		#Settings tab
-		QtCore.QObject.connect(self.ui.pushButton_bt, QtCore.SIGNAL("clicked()"), self.blueman)	#Start Blueman
-		QtCore.QObject.connect(self.ui.spinBox_ATSP, QtCore.SIGNAL("valueChanged(int)"), self.settings)	#ATSP Value
-		
-		if self.ui.btRadio.isChecked():
-			serialDevice = '/dev/rfcomm0'
-		else:
-			serialDevice = '/dev/ttyUSB0'
-		
+        #maybe?? 
+        #QtCore.QObject.connect(self.ui.obdClear, QtCore.SIGNAL("clicked()"), pi2OBD.pi2OBD, QtCore.SLOT("clear_codes()")) #clear codes
+        
+        #GPS tab
+        QtCore.QObject.connect(self.ui.logGPS, QtCore.SIGNAL("clicked()"), self.logGPS)
+        QtCore.QObject.connect(self.ui.GPSbutton, QtCore.SIGNAL("clicked()"), self.GPS)
+        self.stopGPS = False
+        
+        #Settings tab
+        QtCore.QObject.connect(self.ui.pushButton_bt, QtCore.SIGNAL("clicked()"), self.blueman) #Start Blueman
+        QtCore.QObject.connect(self.ui.spinBox_ATSP, QtCore.SIGNAL("valueChanged(int)"), self.settings) #ATSP Value
+        
+        if self.ui.btRadio.isChecked():
+            serialDevice = 'rfcomm0'
+        else:
+            serialDevice = '/dev/ttyUSB0'
+        
 
-#################################################################################################################		
-		
-	def fogL(self):	
-		"""Will turn fog ligths on and off"""
-		if(self.F_state is False ):
-			GPIO.output(self.F_lights, GPIO.HIGH)
-			self.F_state = True
-		else:
-			GPIO.output(self.F_lights, GPIO.LOW)
-			self.F_state = False
-		return 
+#################################################################################################################       
+        
+    def fogL(self):  
+        """Will turn fog ligths on and off"""
+        if(self.F_state is False ):
+            GPIO.output(self.F_lights, GPIO.HIGH)
+            self.F_state = True
+        else:
+            GPIO.output(self.F_lights, GPIO.LOW)
+            self.F_state = False
+        return 
 
-	def fancy(self):
-		"""Will turn fog ligths on and off"""
-		if(self.A_state is False):
-			GPIO.output(self.A_lights, GPIO.HIGH)
-			self.A_state = True
-		else:
-			GPIO.output(self.A_lights, GPIO.LOW)
-			self.A_state = False
-		return
+    def fancy(self):
+        """Will turn fog ligths on and off"""
+        if(self.A_state is False):
+            GPIO.output(self.A_lights, GPIO.HIGH)
+            self.A_state = True
+        else:
+            GPIO.output(self.A_lights, GPIO.LOW)
+            self.A_state = False
+        return
 
-#################################################################################################################	
-	def ODBII(self):
-		"""Starts to read the ODB data"""
-		if not self.stopOBD:
-			self.ui.obdButton.setText("Stop")
-			pi2OBD.pi2OBD().main(False)
-			while not self.stopOBD:
-				obdTemp = open('obdTemp.txt', 'r')
-				obdTemp.seek(self.lineNumber-1)
-				values = obdTemp.readline().split(',')	#TODO When readline is null Still need to know line numbers
-				# receive [speed, rpm, intake, coolant, load]
-				self.ui.lcdNumber_speed.display(values[0])
-				self.ui.lcdNumber_rpm.display(values[1])
-				self.ui.lcdNumber_inTemp.display(values[2])
-				self.ui.lcdNumber_coolTemp.display(values[3])
-				self.ui.lcdNumber_load.display(values[4])
-		else:
-			self.ui.obdButton.setText("Start")
-			pi2OBD.pi2OBD().main(True)	
-		'''#Start thread
-		qtgui.QApplication.processEvents() #Writes all pending changes to QT
-		self.wait(1)
-		updateThread = threading.Thread(target = self.updateThread)
-		updateThread.start()'''
-		self.stopOBD = not self.stopOBD #toggle value
-		return
-		
-	def clearCodes(self):
-		"""Clear all trouble codes"""
-		pi2OBD.pi2OBD.clear_codes()
+#################################################################################################################
+    def ODBII(self):
+        """Starts to read the ODB data"""
+        self.stopOBD = not self.stopOBD
+        OBDvalues = [0, 0, 0, 0, 0]
+        while not self.stopOBD:
+            self.ui.obdButton.setText("Stop")
+            OBDvalues = pi2OBD.pi2OBD().OBDread(OBDvalues) 
+            self.trigger.emit(OBDvalues)
+            PyQt4.QtCore.QCoreApplication.processEvents()
+            
+        self.ui.obdButton.setText("Start")
+        self.trigger.emit(OBDvalues)
+
+        return
+
+    def updateGUI(self, OBDvalues):
+        """Update LCD values in QT: receive [speed, rpm, intake, coolant, load]"""
+        self.ui.lcdNumber_speed.display(OBDvalues[0])
+        self.ui.lcdNumber_rpm.display(OBDvalues[1])
+        self.ui.lcdNumber_inTemp.display(OBDvalues[2])
+        self.ui.lcdNumber_coolTemp.display(OBDvalues[3])
+        self.ui.lcdNumber_load.display(OBDvalues[4])
+        time.sleep(1) #TODO: find better option
+
+        return
+
+    def clearCodes(self):
+        """Clear all trouble codes"""
+        pi2OBD.pi2OBD.clear_codes()
 
 ####################################################################################################
-	def GPS(self):
-		if not self.GPS:
-			self.ui.GPSbutton.setText("Stop")			
-		else:
-			self.ui.GPSbutton.setText("Start")
-		self.stopGPS = not self.stopGPS #toggle value
+    def GPS(self):
+        if not self.GPS:
+            self.ui.GPSbutton.setText("Stop")           
+        else:
+            self.ui.GPSbutton.setText("Start")
+        self.stopGPS = not self.stopGPS #toggle value
 
-	def logGPS(self):
-		pass
+    def logGPS(self):
+        pass
 ####################################################################################################
 
-	def settings(self):
-		"""Function of the settings tab"""
-		self.ATSP = self.ui.spinBox_ATSP.value()
-		
-	def blueman(self):
-		"""Starts blueman-manager"""
-		try:
-			os.system("blueman-manager")
-		except:
-			print "Please install 'blueman' package"		
+    def settings(self):
+        """Function of the settings tab"""
+        self.ATSP = self.ui.spinBox_ATSP.value()
+        
+    def blueman(self):
+        """Starts blueman-manager"""
+        try:
+            os.system("blueman-manager")
+        except:
+            print "Please install 'blueman' package"        
 
 
 if __name__ == "__main__":
